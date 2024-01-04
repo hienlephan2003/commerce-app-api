ec7c1b5043efa625daab0d88e1f3aa5a3a181eb9
Thai Cong Nguyen <21520169@gm.uit.edu.vn>
2024-01-04 09:55:55 +0700

    Fixing Test Coverage

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,13 @@
 module.exports = {
   verbose: true,
   testMatch: [
-    '<rootDir>/src/__tests__/*.test.js',
-    '!<rootDir>/src/__tests__/listOfProductBuy.test.js',
+    '<rootDir>/src/__tests__/auth.test.js',
+    '<rootDir>/src/__tests__/category.test.js',
+    '<rootDir>/src/__tests__/delivery.test.js',
+    '<rootDir>/src/__tests__/discount.test.js',
+    '<rootDir>/src/__tests__/order.test.js',
+    '<rootDir>/src/__tests__/payment.test.js',
+    '<rootDir>/src/__tests__/product.test.js',
+    '<rootDir>/src/__tests__/user.test.js',
   ],
 };
